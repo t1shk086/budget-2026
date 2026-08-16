256fdff86d9b2d157e34d5f80b3518a35ff4cae9
t1shk086 <t.senkov86@gmail.com>
2026-08-17 00:36:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -396,7 +396,7 @@ else:
                         <div style="color: white; margin: 0; font-size: 28px; font-weight: 800; line-height: 1.2; text-align: center;">{total_liters_calculated:.1f} <span style="font-size: 14px; color: #666; font-weight: normal;">литра</span></div>
                     </div>
                     <div style="padding-top: 20px; border-top: 1px solid rgba(255,255,255,0.06); width: 100%;">
-                        <div style="color: #ffa500; font-weight: bold; font-size: 11px; letter-spacing: 0.5px; margin: 0 0 8px 0; text-align: center;">💰 ОБЩА ФИНАНСОВА СТОЙНОСТ</div>
+                        <div style="color: #ffa500; font-weight: bold; font-size: 11px; letter-spacing: 0.5px; margin: 0 0 8px 0; text-align: center;">💰 ОБЩА СТОЙНОСТ</div>
                         <div style="color: white; margin: 0; font-size: 28px; font-weight: 800; line-height: 1.2; text-align: center;">{auto_fuel_money:.2f} <span style="font-size: 14px; color: #666; font-weight: normal;">EUR</span></div>
                     </div>
                 </div>
