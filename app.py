7fb80dfd1df496b4700a1b81a78a5612f5d4d2dd
t1shk086 <t.senkov86@gmail.com>
2026-08-21 23:45:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -672,7 +672,7 @@ else:
 
     if st.button("📊 Разходи по Категории", use_container_width=True, key="open_categories_popup_trigger"):
         разходи_по_категории_dialog()
-        st.markdown("---")
+    st.markdown("---")
     col_st1, col_st2 = st.columns(2)
     with col_st1:
         st.markdown(f"<div style='background:rgba(255,255,255,0.03); border:1px solid rgba(255,255,255,0.1); padding:15px; border-radius:12px; text-align:center; margin-bottom: 12px;'><small style='color:#aaa; font-weight:bold;'>🏨 ДЕПОЗИТ</small><h2 style='color:#ff4b4b; margin:5px 0;'>{depozit_hotel:.2f} <span style='font-size: 14px; font-weight: 500; color: #7e8494;'>EUR</span></h2></div>", unsafe_allow_html=True)
