2907eadc40602e6a7291ba94db1480faa124c3d9
t1shk086 <t.senkov86@gmail.com>
2026-08-23 01:14:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -212,9 +212,9 @@ if st.session_state["current_trip"] is None:
         
         /* ФИКС: Спира разтягането на първата колона и я свива около бутона */
         [data-testid="stHorizontalBlock"] > div:first-child {
-            flex: 0 0 55px !important; /* Заковава ширината на бутона на 55px */
-            min-width: 55px !important;
-            max-width: 55px !important;
+            flex: 0 0 50px !important; /* Заковава ширината на бутона на 55px */
+            min-width: 50px !important;
+            max-width: 50px !important;
         }
 
         /* Изчистен малък текст с перфектна вертикална линия */
