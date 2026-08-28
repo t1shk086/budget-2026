7dff06e066ab3005d4f3f2454daec0ce34b87638
t1shk086 <t.senkov86@gmail.com>
2026-08-28 15:40:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4359,8 +4359,8 @@ div[class*="st-key-trip_card_"] div[data-testid="stButton"] button {
                         SMTP_PORT = 465
 
                         # Тези две стойности се вземат от Streamlit Secrets
-                        SMTP_USERNAME = st.secrets["gmail"]["username"]
-                        SMTP_PASSWORD = st.secrets["gmail"]["app_password"]
+                        SMTP_USERNAME = "t.senkov86@gmail.com"
+                        SMTP_PASSWORD = "abcdefghijklmnop"
 
                         # =================================================
                         # СЪЗДАВЯНЕ НА ИМЕЙЛА
