0422b84601a15cba017f40377f705cc50ad44869
t1shk086 <t.senkov86@gmail.com>
2026-08-28 08:56:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1196,7 +1196,7 @@ if st.session_state["current_trip"] is None:
         }
         /* Подсигурява, че текстът няма да се пречупи на два реда на телефон */
         div[data-testid="stCheckbox"] p {
-            font-size: 14px !important;
+            font-size: 13px !important;
             font-weight: 800 !important;
             letter-spacing: 0 !important;
             color: #aeb5c0 !important;
@@ -1211,7 +1211,7 @@ if st.session_state["current_trip"] is None:
         /* Streamlit понякога поставя текста в допълнителен span */
         div[data-testid="stCheckbox"] label,
         div[data-testid="stCheckbox"] label span {
-            font-size: 11px !important;
+            font-size: 14px !important;
             font-weight: 800 !important;
             letter-spacing: 0 !important;
             color: #9aa1ad !important;
