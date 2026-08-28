ec685fc95331d66b01336b70b6049e165c69832a
t1shk086 <t.senkov86@gmail.com>
2026-08-28 08:55:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1196,7 +1196,7 @@ if st.session_state["current_trip"] is None:
         }
         /* Подсигурява, че текстът няма да се пречупи на два реда на телефон */
         div[data-testid="stCheckbox"] p {
-            font-size: 13px !important;
+            font-size: 14px !important;
             font-weight: 800 !important;
             letter-spacing: 0 !important;
             color: #aeb5c0 !important;
