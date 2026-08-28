e6c394fa9d52e57fa721018d269c87b9b0722049
t1shk086 <t.senkov86@gmail.com>
2026-08-28 09:03:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1209,7 +1209,7 @@ if st.session_state["current_trip"] is None:
         /* Streamlit понякога поставя текста в допълнителен span */
         div[data-testid="stCheckbox"] label,
         div[data-testid="stCheckbox"] label span {
-            font-size: 14px !important;
+            font-size: 13px !important;
             font-weight: 800 !important;
             letter-spacing: 0 !important;
             color: #aeb5c0 !important;
