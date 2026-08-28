6c4e9a11c164bc311f525aab34d2a8d83f176d4f
t1shk086 <t.senkov86@gmail.com>
2026-08-28 15:18:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4279,7 +4279,7 @@ div[class*="st-key-trip_card_"] div[data-testid="stButton"] button {
         
         col_backup1, col_backup2 = st.columns(2)
         
-   python
+   
         with col_backup1:
             st.markdown("##### 📥 Сваляне на архив")
 
